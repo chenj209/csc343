09736c6d4db16241eaa25e02a0daaa8059a3d5af
chenj209 <Jianda.chen@mail.utoronto.ca>
2016-11-12 15:09:32 -0500

    editing violate law

--- A2/q3.sql
+++ A2/q3.sql
@@ -4,13 +4,38 @@ CREATE FUNCTION OVERLAP(listingId int) RETURNS BOOLEAN
 
 CREATE FUNCTION VIOLATELAW(listingId int) RETURNS BOOLEAN
 
-CREATE FUNCTION CHECKDAYS(listingId int, days int, type char(3)) 
-	RETURNS BOOLEAN
-IF type == 'max'
+
+
+CREATE FUNCTION CalculateDays(listingId int, year int) RETURNS int
+AS $$
+BEGIN
+	
+	
+
+CREATE OR REPLACE VIEW BookingWithStartEnd AS 
+SELECT listingId, startdate, (startdate + numNights) AS enddate
+FROM Booking;
+
+CREATE FUNCTION CheckExceed(lId int, days int, type char(3))  
+	RETURNS BOOLEAN AS $$
+BEGIN
+IF (type == 'max')
 THEN
 	IF EXISTS
 		(SELECT *
 		 FROM Booking
-		 WHERE Booking.listingId)
-ELSE
-ENDIF 
+		 WHERE Booking.listingId = lId
+		 	and Booking.numNights > days)
+	THEN RETURN TRUE;
+	END IF;
+ELSIF (type == 'min')
+	IF EXISTS
+		(SELECT *
+		 FROM Booking
+		 WHERE Booking.listingId = lId
+		 	and Booking.numNights < days)
+	THEN RETURN TRUE;
+	END IF;
+END IF; 
+END;
+$$ LANGUAGE plpgsql;
