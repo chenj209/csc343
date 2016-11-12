7897e2d72acd819e8617b769a9590f763b204fe8
chenj209 <Jianda.chen@mail.utoronto.ca>
2016-11-12 16:30:11 -0500

    add violatelaw function

--- A2/q3.sql
+++ A2/q3.sql
@@ -14,40 +14,64 @@ END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION VIOLATELAW(listingId int) RETURNS BOOLEAN
-
-
-
-CREATE FUNCTION CalculateDays(listingId int, year int) RETURNS int
+CREATE FUNCTION VIOLATELAW(lId int, year int) RETURNS BOOLEAN
 AS $$
 BEGIN
-	
-	
+	IF EXISTS 
+		(SELECT * 
+	     FROM CityRegulation, Listing
+	     WHERE Listing.listingId = lId 
+	     AND (Listing.propertyType = CityRegulation.propertyType 
+	     		OR CityRegulation.propertyType = NULL) 
+	     AND CheckExceed(CalculateDays(lId, year), days, RegulationType))
+	THEN RETURN TRUE;
+	ELSE RETURN FALSE;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE VIEW BookingWithStartEnd AS 
-SELECT listingId, startdate, (startdate + numNights) AS enddate
-FROM Booking;
 
-CREATE FUNCTION CheckExceed(lId int, days int, type char(3))  
+CREATE OR REPLACE FUNCTION CalculateDays(lId int, year int) RETURNS int
+AS $$
+DECLARE 
+	endOfYear date;
+	startOfYear date;
+	numDays int;
+BEGIN
+	endOfYear := to_date(to_char(year, '9999') || '-12-31', 'YYYY-MM-DD');
+	startOfYear := to_date(to_char(year, '9999') || '-01-01', 'YYYY-MM-DD');
+	SELECT sum(day) INTO numDays FROM (
+		(SELECT numNights as day
+		 FROM Booking
+		 WHERE listingId = $1 
+		 	and startdate + numNights <= endOfYear
+		 	and startdate >= startOfYear)
+		UNION
+		(SELECT (endOfYear - startdate + 1) as day
+		 FROM Booking
+		 WHERE listingId = $1 
+		 	and startdate + numNights > endOfYear
+		 	and startdate >= startOfYear)
+		UNION
+		(SELECT (startdate - startOfYear + 1) as day
+		 FROM Booking
+		 WHERE listingId = $1 
+		 	and startdate + numNights > startOfYear
+		 	and startdate < startOfYear)
+	) totaldays;
+	RETURN numDays;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION CheckExceed(total int, days int, type char(3))  
 	RETURNS BOOLEAN AS $$
 BEGIN
-IF (type == 'max')
-THEN
-	IF EXISTS
-		(SELECT *
-		 FROM Booking
-		 WHERE Booking.listingId = lId
-		 	and Booking.numNights > days)
+	IF (type == 'max') and (total > days)
 	THEN RETURN TRUE;
-	END IF;
-ELSIF (type == 'min')
-	IF EXISTS
-		(SELECT *
-		 FROM Booking
-		 WHERE Booking.listingId = lId
-		 	and Booking.numNights < days)
-	THEN RETURN TRUE;
-	END IF;
-END IF; 
+	ELSIF (type == 'min') and (total < days)
+ 	THEN RETURN TRUE;
+ 	ELSE RETURN FALSE;
+	END IF; 
 END;
 $$ LANGUAGE plpgsql;
