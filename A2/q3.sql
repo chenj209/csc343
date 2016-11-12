f2f789ae84c02b404216befcd11170d840b2cf89
TonyUUU <tonycl.wu@mail.utoronto.ca>
2016-11-12 16:58:41 -0500

    adding final queries for q3

--- A2/q3.sql
+++ A2/q3.sql
@@ -30,4 +30,31 @@ $$ LANGUAGE plpgsql;
 --		 FROM Booking
 --		 WHERE Booking.listingId)
 --ELSE
---ENDIF 
+--ENDIF
+
+-- Create a view to find all valid properties.
+CREATE OR REPLACE VIEW ValidBooking_q3 AS
+SELECT listingId, startdate, numNights
+FROM Booking 
+WHERE NOT OVERLAP(listingId);
+
+--Create a view for all years.
+CREATE OR REPLACE VIEW AllYears_q3 AS
+SELECT year
+FROM (
+	(SELECT extract(year FROM startdate) AS year
+	FROM Booking) 
+	UNION
+	(SELECT extract(year FROM (Startdate + numNights)) AS year
+	FROM Booking)) BookingYear;
+
+-- Create a view of ValidBooking with years.
+CREATE OR REPLACE VIEW BookingWithYear_q3 AS
+SELECT listingId, startdate, numNights, year
+FROM ValidBooking_q3 JOIN AllYears_q3;
+
+SELECT owner AS homeowner, BookingWithYear_q3.listingId AS listingID,
+	   year, Listing.city AS city
+FROM BookingWithYear_q3 b3 JOIN Listing list
+ON b3.listingId = list.listingId
+WHERE VIOLATELAWS(b3.listingId, b3.year);
