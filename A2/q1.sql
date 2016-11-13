841257faf515c7bf28982af9e5bf8200b105fe8d
chenj209 <Jianda.chen@mail.utoronto.ca>
2016-11-13 11:24:09 -0500

    modifying q1 to not report every last ten years for a traveler if he has not made any request or bookings

--- A2/q1.sql
+++ A2/q1.sql
@@ -1,56 +1,37 @@
 SET search_path TO bnb, public;
 
-
--- View for all years appeared in the table
-CREATE OR REPLACE VIEW AllYears_q1 AS
-SELECT year
-FROM (
-		(SELECT DISTINCT year FROM (
-			SELECT DISTINCT extract(year FROM startdate) AS year
-			FROM BookingRequest) B1)
-			UNION
-		(SELECT DISTINCT year FROM (
-			SELECT DISTINCT extract(year FROM startdate) AS year
-			FROM Booking) B2
-		)
-	) unionofrequestsandbooking;
-
--- View for all travelers with all years
-CREATE OR REPLACE VIEW TravelerWithYear_q1 AS
-SELECT travelerId, email, year
-FROM Traveler, AllYears_q1;
-
 -- View for number of requests for each traveler each year
 CREATE OR REPLACE VIEW NumberOfRequest_q1 AS 
-SELECT TravelerWithYear_q1.travelerId, year, count(requestId) AS numRequests
-FROM BookingRequest RIGHT JOIN TravelerWithYear_q1 
-	ON year = extract(year FROM startdate) 
-	AND BookingRequest.travelerId = TravelerWithYear_q1.travelerId
-WHERE year > 2006 AND year <= 2016
-GROUP BY TravelerWithYear_q1.travelerId, year;
+SELECT Traveler.travelerId, extract(year FROM startdate) as year,
+ count(requestId) AS numRequests
+FROM BookingRequest RIGHT JOIN Traveler 
+	ON BookingRequest.travelerId = Traveler.travelerId
+WHERE (extract(year FROM startdate) > extract(year FROM current_date) - 10 
+AND extract(year FROM startdate) <= extract(year FROM current_date))
+OR startdate IS NULL
+GROUP BY Traveler.travelerId, extract(year FROM startdate);
 
 -- View for number of booking for each traveler each year
 CREATE OR REPLACE VIEW NumberOfBooking_q1 AS 
-SELECT TravelerWithYear_q1.travelerId, year, count(listingId) AS numBooking
-FROM Booking RIGHT JOIN TravelerWithYear_q1 
-	ON year = extract(year FROM startdate)
-	AND Booking.travelerId = TravelerWithYear_q1.travelerId
-WHERE year > 2006 AND year <= 2016
-GROUP BY TravelerWithYear_q1.travelerId, year;
+SELECT Traveler.travelerId, extract(year FROM startdate) as year,
+ count(listingId) AS numBooking
+FROM Booking RIGHT JOIN Traveler 
+	ON Booking.travelerId = Traveler.travelerId
+WHERE (extract(year FROM startdate) > extract(year FROM current_date) - 10 
+AND extract(year FROM startdate) <= extract(year FROM current_date))
+OR startdate IS NULL
+GROUP BY Traveler.travelerId, extract(year FROM startdate);
 
 
 
-
-SELECT TravelerWithYear_q1.travelerId, email, NumberOfRequest_q1.year AS year, 
+SELECT Traveler.travelerId, email, NumberOfRequest_q1.year AS year, 
 	numRequests, numBooking
-FROM TravelerWithYear_q1 JOIN
+FROM Traveler LEFT JOIN
 	(
 	NumberOfRequest_q1 JOIN NumberOfBooking_q1  
-	ON (NumberOfRequest_q1.travelerId = NumberOfBooking_q1.travelerId
-		AND NumberOfRequest_q1.year = NumberOfBooking_q1.year) 
+	ON NumberOfRequest_q1.travelerId = NumberOfBooking_q1.travelerId
 	)
-	ON TravelerWithYear_q1.travelerId = NumberOfRequest_q1.travelerId
-		AND TravelerWithYear_q1.year = NumberOfRequest_q1.year
+	ON Traveler.travelerId = NumberOfRequest_q1.travelerId
 ORDER BY NumberOfRequest_q1.year DESC;
 
 
