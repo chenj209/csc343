d7128dddc8af325b36680a48e96f9397f703c200
chenj209 <Jianda.chen@mail.utoronto.ca>
2016-11-14 15:18:50 -0500

    change integer division to real division

--- A2/q4.sql
+++ A2/q4.sql
@@ -13,7 +13,7 @@ FROM ValidContributor_q4 v1 JOIN ValidContributor_q4 v2
 ON v1.owner = v2.owner AND v1.year < v2.year
 WHERE v1.avgRating > v2.avgRating;
 
-SELECT to_char((count(distinct v.owner) - count(distinct n.owner)) / count(distinct v.owner) * 100, '999.99') || '%' AS percentage
+SELECT to_char((count(distinct v.owner) - count(distinct n.owner))::real / count(distinct v.owner) * 100, '999.99') || '%' AS percentage
 FROM ValidContributor_q4 v FULL JOIN NonImproveOwner_q4 n
 ON v.owner = n.owner;
 
