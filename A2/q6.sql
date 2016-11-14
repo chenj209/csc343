ea75d388924ba64c3ef896135107b0d03f6a2ca4
chenj209 <Jianda.chen@mail.utoronto.ca>
2016-11-14 17:31:15 -0500

    modifying q6 interface

--- A2/q6.sql
+++ A2/q6.sql
@@ -1,9 +1,11 @@
 SET search_path TO bnb, public;
 
 -- Create a view to find all the Requests made by each traveler.
-CREATE VIEW OR REPLACE TravelerRequest AS
+CREATE OR REPLACE VIEW TravelerRequest AS
 SELECT tavelerId, listingId
 
 -- Create a view to find all the Booking made by each traveler.
-CREATE VIEW OR REPLACE TravelerBooking AS
+CREATE OR REPLACE VIEW TravelerBooking AS
 SELECT travelerId, listingId
+
+
