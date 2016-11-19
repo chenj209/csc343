286af3865dd888718dc2b8626ecc5d2b539fc477
chenj209 <Jianda.chen@mail.utoronto.ca>
2016-11-19 18:47:47 -0500

    add final version of part2 query for homeowner 4000

--- A2/part2.sql
+++ A2/part2.sql
@@ -13,8 +13,43 @@ SELECT T1.travelerId, T1.homeownerId, (SELECT avg(rating) AS AvgRating
 								 	   FROM TravelerOwnerRating
 								       WHERE travelerId = T1.travelerId AND homeownerId = T1.homeownerId
 								       GROUP BY travelerId, homeownerId
-								       ) 
+								       ) AS avgRating
 FROM TravelerOwnerMatrix T1;
 
-SELECT * FROM AvgRatingMaxtirx;
+CREATE OR REPLACE VIEW AvgRatingWithProduct AS
+SELECT homeownerId, (coalesce(avgRating, 0) * (SELECT avgRating
+								  FROM AvgRatingMaxtirx
+								  WHERE travelerId = A.travelerId
+								  AND homeownerId = 4000) ) AS product
+WHERE homeownerId <> 4000
+FROM AvgRatingMaxtirx A;
+
+CREATE OR REPLACE VIEW Similarity AS
+SELECT homeownerId, sum(product) AS score
+FROM AvgRatingWithProduct
+GROUP BY homeownerId
+ORDER BY similarity DESC;
+
+CREATE OR REPLACE VIEW TopTen AS
+SELECT homeownerId, score
+FROM Similarity
+LIMIT 10;
+
+CREATE OR REPLACE VIEW TieTen AS
+SELECT homeownerId, score
+FROM Similarity
+WHERE score = (SELECT score
+			   FROM TopTen
+			   ORDER BY score ASC
+			   LIMIT 1) ten;
+
+SELECT homeownerId, score
+FROM (SELECT * FROM TopTen) UNION (SELECT * FROM TieTen)
+ORDER BY score DESC;
+
+
+
+
+
+
 
