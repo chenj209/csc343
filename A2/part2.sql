9276de0b32f54803689fc081476e31f883d4ee32
chenj209 <Jianda.chen@mail.utoronto.ca>
2016-11-19 17:50:42 -0500

    adding part2 query

---
+++ A2/part2.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW TravelerOwnerMatrix AS
+SELECT travelerId, homeownerId
+FROM Traveler, Homeowner;
+
+CREATE OR REPLACE VIEW TravelerOwnerRating AS
+SELECT travelerId, owner AS homeownerId, rating 
+FROM TravelerRating JOIN Listing ON TravelerRating.listingId = Listing.listingId
+
+CREATE OR REPLACE VIEW AvgRatingMaxtirx AS
+SELECT T1.travelerId, T1.homeownerId, (SELECT avg(rating) AS AvgRating
+								 	   FROM TravelerOwnerRating
+								       WHERE travelerId = T1.travelerId AND homeownerId = T1.homeownerId
+								       GROUP BY travelerId, homeownerId
+								       ) 
+FROM TravelerOwnerMatrix T1
+
+SELECT * FROM AvgRatingMaxtirx;
+
